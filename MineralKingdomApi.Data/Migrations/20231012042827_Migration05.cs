8253de065c3599127d3665717a9427bf2d9d146c
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2023-10-11 23:38:42 -0500

    Add dotEnv Add gitignore file

---
+++ MineralKingdomApi.Data/Migrations/20231012042827_Migration05.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MineralKingdomApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration05 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Auctions_Minerals_MineralId",
+                table: "Auctions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MineralId",
+                table: "Auctions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Auctions_Minerals_MineralId",
+                table: "Auctions",
+                column: "MineralId",
+                principalTable: "Minerals",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Auctions_Minerals_MineralId",
+                table: "Auctions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MineralId",
+                table: "Auctions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Auctions_Minerals_MineralId",
+                table: "Auctions",
+                column: "MineralId",
+                principalTable: "Minerals",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
