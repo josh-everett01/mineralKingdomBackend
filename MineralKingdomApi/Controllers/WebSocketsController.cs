45a4b7cd138f61ed4154b023a0c4fdccbf557b20
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2024-01-22 16:52:35 -0600

    Add Websocket
    This commit adds a websocket to the project
    At the moment we use this to know when a new bid has been placed in order
    to refresh the Auctions bid list on the AuctionDetails view on the front end.

---
+++ MineralKingdomApi/Controllers/WebSocketsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using MineralKingdomApi.Services;
+using MineralKingdomApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WebSocketsController : ControllerBase
+{
+    private readonly AppWebSocketsManager _appWebSocketManager;
+    private readonly ILogger<WebSocketsController> _logger;
+
+    public WebSocketsController(AppWebSocketsManager appWebSocketManager, ILogger<WebSocketsController> logger)
+    {
+        _appWebSocketManager = appWebSocketManager;
+        _logger = logger;
+    }
+
+    [HttpGet("/ws")]
+    public async Task Get()
+    {
+        _logger.LogInformation(HttpContext.TraceIdentifier);
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            var id = Guid.NewGuid(); // Generate a unique ID for the socket
+            await _appWebSocketManager.Handle(id, webSocket);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = 400;
+        }
+    }
+}
